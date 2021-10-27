bbc73adfc29fd7eb8e85fe9d009c39deb5ec3822
Salman J. Khan <gravity9@Salmans-MacBook-Air.local>
2021-10-27 19:10:36 -0400

    initial tf files upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region =  "us-east-2"
+    access_key = "AKIA2XSUNFG6PVIUZHDM"
+    secret_key = "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"
+}
+
+variable "cidr_blocks" {
+    description = "cidr blocks for vpc and subnet"  
+    type = list(string)
+}
+
+resource "aws_vpc" "development_vpc" {
+    cidr_block = var.cidr_blocks[0]
+    tags = {
+        Name: "development"
+    } 
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development_vpc.id
+    cidr_block = var.cidr_blocks[1]
+    availability_zone = "us-east-2a"  
+    tags = {
+        Name: "subnet-1-dev"
+    } 
+}
+
+output "dev-vpc-id" {
+    value = aws_vpc.development_vpc.id 
+}
+
+output "dev-subnet-id" {
+    value = aws_subnet.dev-subnet-1.id  
+}
